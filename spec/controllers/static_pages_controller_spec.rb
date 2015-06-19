8e9a9ec3d4eed1fe3843023aeccb178e3fb98ecf
sjahed <hussini@jahed.me>
2015-06-19 15:27:58 -0400

    finished static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe StaticPagesController do
 
   describe "GET 'home'" do
+  	  render_views
     it "returns http success" do
       get 'home'
       response.should be_success
@@ -10,10 +11,18 @@ describe StaticPagesController do
   end
 
   describe "GET 'help'" do
+  	  render_views
     it "returns http success" do
       get 'help'
       response.should be_success
     end
   end
 
+  describe "GET 'about'" do
+  	  render_views
+  	  it "returns http success" do
+  	  	  get 'about'
+  	  	  response.should be_success
+	  end
+  end
 end
